4c09dc639de0b465e3679d3527609fc0786a7811
Ruly <30475401@qq.com>
2016-04-27 22:17:31 +0800

    加了grid

---
+++ Project/Assets/Sample/Demo/SnapGrid.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapGrid : MonoBehaviour
+{
+	public const float gridSize = 1f;
+	void Awake()
+	{
+		if (Application.isPlaying)
+		{
+			Object.Destroy (this);
+		}
+	}
+	void Update()
+	{
+		var pos = transform.position;
+		pos.x = pos.x % gridSize;
+		pos.y = pos.y % gridSize;
+		pos.z = pos.z % gridSize;
+		transform.position = pos;
+	}
+}
