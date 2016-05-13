f4041783082f455881c6e69693da9daac0a4b324
Ruly <30475401@qq.com>
2016-05-14 07:10:39 +0800

    调整空降时长.

--- Project/Assets/Sample/Demo4/Script/Mine.cs
+++ Project/Assets/Sample/Demo4/Script/Mine.cs
@@ -5,19 +5,5 @@ namespace Demo4
 {
 	public class Mine : MonoBehaviour
 	{
-		public ResourceType type = ResourceType.Power;
-		public Refinery refineryPrefab;
-		private Refinery m_refineryIns = null;
-		public bool CreateRefinery()
-		{
-			if (null == m_refineryIns && refineryPrefab!=null)
-			{
-				var go = Object.Instantiate
-					(refineryPrefab.gameObject) as GameObject;
-
-				m_refineryIns = go.GetComponent<Refinery> ();
-			}
-			return false;
-		}
 	}
 }
