ebac48eb4b7f983d422c3f07b356af595dfb5580
Ruly <30475401@qq.com>
2016-05-14 07:46:34 +0800

    add camera track

---
+++ Project/Assets/Sample/Demo4/Script/CameraTrack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Demo4
+{
+	public class CameraTrack : MonoBehaviour
+	{
+		public Transform target;
+		public Transform cameraAt;
+		public float tweenScale= 0.3f;
+		void Start()
+		{
+			cameraAt.position = target.position;
+		}
+		void Update ()
+		{
+			if (target != null && cameraAt != null)
+			{
+				cameraAt.position = Vector3.Lerp
+					(cameraAt.position, target.position, tweenScale);
+			}
+		}
+	}
+}
