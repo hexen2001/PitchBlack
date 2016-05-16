31339f81517c6ce7daeeb55a9a1989e6f6dc5ffe
Ruly <30475401@qq.com>
2016-05-16 16:05:19 +0800

    添加输入事件

---
+++ Project/Assets/Sample/Demo4/Script/Trigger/Event/EventInputKey.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Demo4
+{
+	public class EventInputKey : Event
+	{
+		public enum Mode
+		{
+			Key,
+			KeyDown,
+			KeyUp,
+		}
+		public Mode mode = Mode.KeyDown;
+		public KeyCode key;
+		protected void Update()
+		{
+			switch (mode)
+			{
+			case Mode.Key:
+				if (Input.GetKey (key))
+				{
+					Raise ();
+				}
+				break;
+
+			case Mode.KeyDown:
+				if (Input.GetKeyDown (key))
+				{
+					Raise ();
+				}
+				break;
+
+			case Mode.KeyUp:
+				if (Input.GetKeyUp (key))
+				{
+					Raise ();
+				}
+				break;
+
+			}
+		}		
+	}
+}
