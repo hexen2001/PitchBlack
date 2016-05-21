56ff561c18928f32ba16baef9804d7acf6644ec7
Ruly <30475401@qq.com>
2016-05-21 11:06:43 +0800

    添加建筑地基

---
+++ Project/Assets/Sample/Demo4/Script/Building.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Demo4
+{
+	public class Building : MonoBehaviour
+	{
+		/// <summary>
+		/// 实例引用
+		/// </summary>
+		public GameObject buildingInstance = null;
+
+		/// <summary>
+		/// 构造清单
+		/// </summary>
+		public BuildingList makeList;
+
+		/// <summary>
+		/// 状态
+		/// </summary>
+		public enum State
+		{
+			//	空地,什么也没有
+			Empty,
+
+			//	装载中
+			Launcher,
+
+			//	正在运行中
+			Running,
+		}
+		public State state;
+		public Title title;
+		/// <summary>
+		/// 按指定Info造物
+		/// </summary>
+		public void Create(BuildingList.MakeInfo makeInfo)
+		{
+			if (state == State.Empty)
+			{
+				state = State.Launcher;
+				StartCoroutine (CreateTask(makeInfo));
+			}
+		}
+		private IEnumerator CreateTask(BuildingList.MakeInfo makeInfo)
+		{
+			float endTime = Time.time + makeInfo.time;
+			while( Time.time < endTime )
+			{
+				float scale = (endTime - Time.time) / makeInfo.time;
+				title.text = (int)( scale * 100 ) + "%";
+				yield return new WaitForEndOfFrame ();
+			}
+			buildingInstance = makeInfo.prefab.Create (transform);
+			state = State.Running;
+			while(buildingInstance!=null)
+			{
+				yield return new WaitForSeconds (1f);
+			}
+			state = State.Empty;
+		}
+
+		#region 应答机制
+		void OnTriggerEnter(Collider collider)
+		{
+			if ( CheckIsMarine (collider))
+			{
+				dialogBuildingList.Add (this);
+			}
+		}
+		void OnTriggerExit(Collider collider)
+		{
+			if ( CheckIsMarine (collider))
+			{
+				dialogBuildingList.Add (this);
+			}
+		}
+		private static List<Building> dialogBuildingList
+		= new List<Building> ();
+		private bool CheckIsMarine(bool collider)
+		{
+			return false;
+		}
+		#endregion
+
+		#region Debug
+		public List<Building> debugShowDialogList;
+		protected void Update()
+		{
+			debugShowDialogList = dialogBuildingList;
+		}
+		#endregion
+
+	}
+}
---
+++ Project/Assets/Sample/Demo4/Script/BuildingList.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Demo4
+{
+	/// <summary>
+	/// 建筑列表
+	/// </summary>
+	public class BuildingList : MonoBehaviour
+	{
+		/// <summary>
+		/// 建造数据
+		/// 每个代表一项可召唤物
+		/// </summary>
+		[System.Serializable]
+		public class MakeInfo
+		{
+			public GameObject prefab = null;
+			public float time = 1.5f;
+			public int priceMetal = 0;
+			public int pricePower = 0;
+		}
+
+		/// <summary>
+		/// 可造物清单
+		/// </summary>
+		public MakeInfo[] nodes;
+
+	}
+}
